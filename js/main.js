39e052c145cb5d9f24a68d290bdc560b15d45946
jessicadraper <jessicafaye.draper@gmail.com>
2025-09-11 05:39:43 -0700

    Fixed and refactored wikiebears.js to use async/await

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import { test } from './modules/search.js'
 import { toggleComments, commentForm } from './modules/comments.js';
-import { bears } from './modules/wikibear.js';
+import { wikibears } from './modules/wikibear.js';
 
 test();
 toggleComments();
 commentForm();
-bears();
+wikibears();
--- js/modules/wikibear.js
+++ js/modules/wikibear.js
@@ -1,12 +1,14 @@
 // wikibears.js
 
 // Fetching bear data 
-function bears() {
+export async function wikibears() {
 
-    var baseUrl = "https://en.wikipedia.org/w/api.php";
-    var title = "List_of_ursids";
+    const PLACEHOLDER_IMAGE = '/media/bear-placeholder.jpg';
 
-    var params = {
+    const baseUrl = "https://en.wikipedia.org/w/api.php";
+    const title = "List_of_ursids";
+
+    const params = {
         action: "parse",
         page: title,
         prop: "wikitext",
@@ -15,8 +17,8 @@ function bears() {
         origin: "*"
     };
 
-    function fetchImageUrl(fileName) {
-        var imageParams = {
+    async function fetchImageUrl(fileName) {
+        const imageParams = {
             action: "query",
             titles: "File:" + fileName,
             prop: "imageinfo",
@@ -25,65 +27,85 @@ function bears() {
             origin: "*"
         };
 
-        var url = baseUrl + "?" + new URLSearchParams(imageParams).toString();
+        const url = baseUrl + "?" + new URLSearchParams(imageParams).toString();
         
-        return fetch(url).then(function(res) {
-            return res.json();
-        }).then(function(data) {
-            var pages = data.query.pages;
-            var page = Object.values(pages)[0];
-            return page.imageinfo[0].url;
-        });
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+
+            const page = Object.values(data?.query?.pages || {})[0];
+            return page?.imageinfo?.[0]?.url || PLACEHOLDER_IMAGE;
+        }
+        catch (e) {
+            console.log(e);
+            return PLACEHOLDER_IMAGE
+        }
     }
 
-    function extractBears(wikitext) {
-        console.log(wikitext);
-        var speciesTables = wikitext.split('{{Species table/end}}');
-        var bears = [];
+    async function extractBears(wikitext) {
+        const speciesTables = wikitext.split('{{Species table/end}}');
+        const bears = [];
 
-        speciesTables.forEach(function(table) {
-            var rows = table.split('{{Species table/row');
+        for (const table of speciesTables) {
+            const rows = table.split('{{Species table/row');
             
-            rows.forEach(function(row) {
-                var nameMatch = row.match(/\|name=\[\[(.*?)\]\]/);
-                var binomialMatch = row.match(/\|binomial=(.*?)\n/);
-                var imageMatch = row.match(/\|image=(.*?)\n/);
-                var rangeMatch = row.match(/\|range=(.*?)\|/);
+            for (const row of rows) {
+                const nameMatch = row.match(/\|name=\[\[(.*?)\]\]/);
+                const binomialMatch = row.match(/\|binomial=(.*?)\n/);
+                const imageMatch = row.match(/\|image=(.*?)\n/);
+                const rangeMatch = row.match(/\|range=(.*?)\|/);
 
-                if (nameMatch && binomialMatch && imageMatch && rangeMatch) {
-                    var fileName = imageMatch[1].trim().replace('File:', '');
+                // Skip if required data is missing
+                if (!nameMatch || !binomialMatch || !rangeMatch) continue;
 
-                    fetchImageUrl(fileName).then(function(imageUrl) {
-                        var bear = {
-                            name: nameMatch[1],
-                            binomial: binomialMatch[1],
-                            image: imageUrl,
-                            range: rangeMatch[1]
-                        };
-                        bears.push(bear);
-                        console.log("BEARS: " + bears.length);
+                const name = nameMatch[1];
+                const binomial = binomialMatch[1];
+                const range = rangeMatch[1];
 
-                        if (bears.length === 8) {
-                            console.log("DONE! Print to page...");
-                            var moreBears = document.querySelector('.more_bears');
-                            bears.forEach(function(bear) {
-                                var html = '<div class="bear">' +
-                                    '<img src="' + bear.image + '" alt="Image of ' + bear.name + '" style="width:200px; height:auto;">' +
-                                    '<p><b>' + bear.name + '</b> (' + bear.binomial + ')</p>' +
-                                    '<p>Range: ' + bear.range + '</p>' +
-                                    '</div>';
-                                moreBears.innerHTML += html;
-                            });
-                        }
-                    });
+                let imageUrl = PLACEHOLDER_IMAGE;
+
+                if (imageMatch) {
+                    const fileName = imageMatch[1].trim().replace('File:', '');
+
+                    // Fetch url and check if broken/available; otherwise placeholder
+                    try {
+                        const fetchedImageUrl = await fetchImageUrl(fileName);
+                        imageUrl = await checkImageAvailability(fetchedImageUrl);
+                    } catch (error) {
+                        imageUrl = PLACEHOLDER_IMAGE; // placeholder image if fetch or check fails
+                    }
                 }
-            });
-        });
+
+                // Add bear to array
+                const bear = {
+                    name,
+                    binomial,
+                    image: imageUrl,
+                    range
+                };
+                bears.push(bear);
+            }
+        }
+        return bears;
+    }
+
+    async function checkImageAvailability(url) {
+        try {
+            const response = await fetch(url, { method: 'GET' });
+            if (response.ok) {
+                // console.log("URL available: ", url)
+                return url;
+            } else {
+                // console.log("URL not available: ", url)
+                return PLACEHOLDER_IMAGE;
+            }
+        } catch {
+            return PLACEHOLDER_IMAGE;
+        }
     }
 
     function printBears(bears) {
-        console.log("DONE! Print to page...");
-        var moreBears = document.querySelector('.more_bears');
+        const moreBears = document.querySelector('.more_bears');
         bears.forEach(function(bear) {
             var html = '<div class="bear">' +
                 '<img src="' + bear.image + '" alt="Image of ' + bear.name + '" style="width:200px; height:auto;">' +
@@ -94,14 +116,22 @@ function bears() {
         });
     }
 
-    fetch(baseUrl + "?" + new URLSearchParams(params).toString())
-    .then(function(res) { 
-        return res.json(); 
-    })
-    .then(function(data) {
-        extractBears(data.parse.wikitext['*']);
-    });
+    async function getAndPrintBears(params) {
+        try {
+            const url = baseUrl + "?" + new URLSearchParams(params).toString();
+            const res = await fetch(url);
+            const data = await res.json()
+
+            console.log("Getting bears...");
+            const bears = await extractBears(data.parse.wikitext['*']);
+
+            console.log("Bears loaded!");
+            printBears(bears)
+        } catch (error) {
+            console.error("Error loading bears: ", error)
+        }
+    }
+
+    getAndPrintBears(params)
 
 }
-
-export {bears}
